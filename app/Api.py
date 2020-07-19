ed09da9d78ba88520932439c292915ff262f7a06
Kaiofprates <kaioprates@outlook.com>
2020-07-19 14:22:37 -0300

    add profile and followees

--- app/Api.py
+++ app/Api.py
@@ -19,14 +19,26 @@ class Insta:
     def get_followers(self):
         try:  
             self.profile = instaloader.Profile.from_username(self.L.context, self.username) 
+
+            info = { 'profile_image' : self.profile.get_profile_pic_url() , 'biography' : self.profile.biography }
+
             followers = set(self.profile.get_followers()) 
             followers = list(followers)
-            seguidores = []
+            seguidores = []  
 
             for i in range(len(followers)):
                 seguidores.append(str(followers[i]).split(' ')[1])
+
+            followes = set(self.profile.get_followees())
+            followes = list(followes)
+            seguindo = []
+
+            for i in range(len(followes)):
+                seguindo.append(str(followes[i]).split(' ')[1])
+            
             if(seguidores):
-                return ({ 'data' : seguidores })    
+                return ({ 'profile': info ,'followers' : seguidores , 'followees' : seguindo })    
+
         except:
             return({ 'info' : 'Error connecting to your account '})    
 
